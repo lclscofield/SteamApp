2276e499a70165272cbccd10b7c10c1a9ecee210
lclscofield <lclscofield@foxmail.com>
2020-03-14 17:24:32 +0800

    feat: 引入 tabs 组件，完成列表组件

---
+++ src/components/scroll-list/index.js
@@ -0,0 +1,13 @@
+Component({
+    properties: {
+        gameList: {
+            // 游戏列表
+            type: Array,
+            value: []
+        }
+    },
+
+    attached () {
+        // console.log(this.data.gameList)
+    }
+})
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,33 @@
 //index.js
 const app = getApp()
 
+const exp = {
+    imgMin: 'https://media.st.dl.eccdnx.com/steam/apps/359550/header.jpg',
+    title: "Tom Clancy's Rainbow Six® Siege",
+    url: 'https://store.steampowered.com/app/359550/Tom_Clancys_Rainbow_Six_Siege/',
+    gId: '359550',
+    released: '2015年12月1日',
+    reviewImg: 'positive',
+    discount: 60,
+    price: 88,
+    strike: 35
+}
+
+const list = [
+    {
+        title: '实时优惠',
+        type: 'discount'
+    },
+    {
+        title: '最热游戏',
+        type: 'hot'
+    },
+    {
+        title: '热门新品',
+        type: 'new'
+    }
+]
+
 Page({
     data: {
         tabs: [],
@@ -8,8 +35,13 @@ Page({
     },
 
     onLoad() {
-        const titles = ['折扣', '热门']
-        const tabs = titles.map(item => ({ title: item }))
+        const tabs = list.map(item => {
+            return {
+                title: item.title,
+                type: item.type,
+                list: Array(100).fill(exp)
+            }
+        })
         this.setData({ tabs })
     },
 
