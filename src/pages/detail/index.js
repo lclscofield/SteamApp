329da3b80c46866e12982b50451f737907f6432a
lclscofield <lclscofield@foxmail.com>
2020-03-23 18:49:12 +0800

    feat: detail 页一下初始逻辑

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -2,12 +2,27 @@ const db = require('../../db/index')
 
 Page({
     data: {
-        detail: {} // 游戏详情
+        detail: {}, // 游戏详情
+        currentImg: '' // 当前主图
     },
 
     async onLoad(query) {
         console.log(query)
         const { gId } = query
         const res = await db.fetchGameDetail(gId)
+        if (res) {
+            this.setData({
+                detail: res
+            })
+            wx.setNavigationBarTitle({
+                title: res.appName || '详情'
+            })
+            this.init()
+        }
+    },
+
+    // 初始化
+    init() {
+        
     }
 })
