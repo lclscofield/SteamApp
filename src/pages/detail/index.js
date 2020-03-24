d5b74874dafff5d6009796012d713e432f32e8b3
lclscofield <lclscofield@foxmail.com>
2020-03-24 15:53:45 +0800

    feat: 完成大部分详情页

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -3,7 +3,8 @@ const db = require('../../db/index')
 Page({
     data: {
         detail: {}, // 游戏详情
-        currentImg: '' // 当前主图
+        currentMediaIdx: 0, // 当前媒体元素
+        media: []  // 媒体数据，包括图片和视频，视频在前
     },
 
     async onLoad(query) {
@@ -23,6 +24,46 @@ Page({
 
     // 初始化
     init() {
-        
+        console.log(this.data.detail)
+        const { imgs, videos } = this.data.detail
+        const media = []
+
+        videos.forEach(item => {
+            media.push({
+                type: 'video',
+                src: item.src,
+                poster: item.poster
+            })
+        })
+        imgs.forEach(item => {
+            media.push({
+                type: 'img',
+                src: item.imgMin,
+                poster: item.imgMin,
+                imgMax: item.imgMax
+            })
+        })
+
+        this.setData({
+            media
+        })
+    },
+
+    // 切换当前 media
+    switchMedia (e) {
+        console.log(e)
+        const idx = e.currentTarget.id
+        this.setData({
+            currentMediaIdx: Number(idx)
+        })
+    },
+
+    // 预览图片
+    preview (e) {
+        console.log(e)
+        const imgMaxUrl = e.currentTarget.id
+        wx.previewImage({
+            urls: [imgMaxUrl]
+        })
     }
 })
